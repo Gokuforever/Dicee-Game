6e061c2077f1416ca04449568b6a5b330ecc5491
Gokuforever <92438930+Gokuforever@users.noreply.github.com>
2022-03-05 13:35:12 +0530

    Random push

---
+++ Learning/day1.js
@@ -0,0 +1,35 @@
+// const heading = document.getElementById("heading");
+// console.log(heading);
+// const heading = document.getElementsByTagName("h1");
+// const heading = document.getElementsByClassName("heading")
+// const heading = document.querySelector(".heading");
+// const parent = heading.parentNode;
+// console.log(parent);
+// const parent = document.querySelector(".parent");
+// const child = parent.childNodes;
+// console.log(child);
+// const heading = document.querySelector("h1");
+// const sibling = heading.nextElementSibling;
+// console.log(sibling);
+// const subHeading = document.querySelector("h3");
+// const sibling = subHeading.previousElementSibling;
+// console.log(sibling);
+// const heading = document.querySelector(".heading");
+// heading.innerHTML = "Updated HTML";
+// heading.classList.add("title");
+// heading.classList.remove("title");
+// const heading = document.createElement('h1');
+// heading.innerHTML = "JavaScript is Awesome!";
+// const parent = document.querySelector(".parent");
+// parent.appendChild(heading);
+// const subHeading = document.createElement("h3");
+// subHeading.innerHTML = "Web Dev is awesome!";
+// heading.insertAdjacentElement("beforebegin",subHeading);
+// const heading = document.querySelector("#heading");
+// const button = document.querySelector("#btn");
+
+// button.addEventListener("click", function(event){
+//     console.log("Button is CLicked!", event);
+//     heading.style.color = "red";
+//     heading.style.fontSize = "10rem";
+// });
